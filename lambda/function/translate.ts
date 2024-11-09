9895e88c22211ce9fcffd1e6086ed306317a31e0
Enes <102901204+mec-cs@users.noreply.github.com>
2024-11-09 17:41:52 +0300

    Improve translation API and update game type structure

--- lambda/function/translate.ts
+++ lambda/function/translate.ts
@@ -1,36 +1,94 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import 'source-map-support/register';
 import apiResponses from '../common/apiResponses';
 import * as AWS from 'aws-sdk';
+import { Game } from "../../shared/types";
 
 const translate = new AWS.Translate();
+const ddbDocClient = createDDbDocClient();
+
+const LanguageMap = {
+    "English": "en",
+    "French": "fr",
+    "Spanish": "es",
+    "German": "de",
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
 
-    const body = JSON.parse(event.body || "{}");
-    const { text, language } = body;
+    const { userId, gameId } = event.pathParameters || {};
+    const language = event.queryStringParameters?.language;
 
-    if (!text) {
-        return apiResponses._400({ message: 'missing text fom the body' });
+    if (!userId || !gameId) {
+        return apiResponses._400({ message: "Missing ID parameter." });
     }
 
     if (!language) {
-        return apiResponses._400({ message: 'missing language from the body' });
+        return apiResponses._400({ message: "Missing language from the body" });
     }
 
+    if (!(language in LanguageMap)) {
+        return apiResponses._400({ message: "Invalid translation language, please give a valid language!" });
+    }
+
+    // var will be the code of that language 
+    const languageCode = LanguageMap[language as keyof typeof LanguageMap];
+
     try {
-        const translateParams: AWS.Translate.Types.TranslateTextRequest = {
-            Text: text,
-            SourceLanguageCode: 'en',
-            TargetLanguageCode: language,
-        };
+        const gameData = await ddbDocClient.send(
+            new GetCommand({
+                TableName: process.env.GAMES_TABLE,
+                Key: { userId, gameId }
+            })
+        );
 
-        const translatedMessage = await translate.translateText(translateParams).promise();
+        if (!gameData.Item) {
+            return apiResponses._404({ message: `Game with ID ${gameId} not found for user ${userId}.` });
+        }
 
-        return apiResponses._200({ translatedMessage });
+        const game: Game = gameData.Item as Game;
+
+        // if game has a translation and that translation is selected language, return it without any process
+        if (game.sourceLanguage === languageCode) {
+            return apiResponses._200({ language: language, translated: "false", game: game });
+        }
+
+        // translation order : title -> genre -> description
+        const fieldsToTranslate = ["title", "genre", "description"];
+
+        for (let field of fieldsToTranslate) {
+            const translateParams: AWS.Translate.Types.TranslateTextRequest = {
+                Text: game[field],
+                SourceLanguageCode: game.sourceLanguage,
+                TargetLanguageCode: languageCode,
+            };
+
+            const translateMessage = await translate.translateText(translateParams).promise();
+            game[field] = translateMessage.TranslatedText;
+        }
+
+        // to check updated game data
+        console.log("[TRANSLATE ITEM]", JSON.stringify({ gameId: gameId, game: game }));
+        return apiResponses._200({ language: language, translated: "true", game: game });
 
     } catch (error: any) {
         console.log('error in the translation', error);
         return apiResponses._400({ message: 'unable to translate the message' });
     }
 };
+
+function createDDbDocClient() {
+    const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+    const marshallOptions = {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+        convertClassInstanceToMap: true,
+    };
+    const unmarshallOptions = {
+        wrapNumbers: false,
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
+    return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
